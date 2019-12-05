5c6bbda41e51550580e1dbbd8a848fde7fbbf436
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-12-05 11:35:43 +0800

    Add directory traversal challenge

---
+++ directory-traversal/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
+const port = process.env.NJSPORT || 10000
+app.use(morgan('combined'))
+app.get('/public', (req, res) => {
+  const pathQuery = req.query.file
+  if (!pathQuery) {
+    res.redirect('/public/?file=README.md')
+  } else {
+    const filePath = path.resolve(`${__dirname}/${pathQuery}`)
+    res.sendFile(filePath, { dotfiles: 'allow' })
+  }
+})
+app.get('/robots.txt', (req, res) => {
+  res.sendfile('robots.txt', { root: __dirname })
+})
+app.listen(port, () => { console.log(`Directory traversal demo up @ ${port}.`) })
