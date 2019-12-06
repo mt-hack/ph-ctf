3a2492a7b82e93969dc29d57b93b78b78f3cc19a
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-12-06 15:31:41 +0800

    Redirect root route

--- directory-traversal/app/app.js
+++ directory-traversal/app/app.js
@@ -4,7 +4,14 @@ const path = require('path')
 const app = express()
 const port = process.env.NJSPORT || 10000
 app.use(morgan('combined'))
-app.get('/public', (req, res) => {
+app.get('/', getFile)
+app.get('/public', getFile)
+app.get('/robots.txt', (req, res) => {
+  res.sendFile('robots.txt', { root: __dirname })
+})
+app.listen(port, () => { console.log(`Directory traversal demo up @ ${port}.`) })
+
+function getFile (req, res) {
   const pathQuery = req.query.file
   if (!pathQuery) {
     res.redirect('/public/?file=README.md')
@@ -12,8 +19,4 @@ app.get('/public', (req, res) => {
     const filePath = path.resolve(`${__dirname}/${pathQuery}`)
     res.sendFile(filePath, { dotfiles: 'allow' })
   }
-})
-app.get('/robots.txt', (req, res) => {
-  res.sendFile('robots.txt', { root: __dirname })
-})
-app.listen(port, () => { console.log(`Directory traversal demo up @ ${port}.`) })
+}
