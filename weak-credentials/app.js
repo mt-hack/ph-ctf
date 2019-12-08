1ce13023c328e918b82a7b6160acc456a871ef99
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-12-08 17:40:34 +0800

    Finalize weak-credential
    Signed-off-by: Still Hsu <5843208+Still34@users.noreply.github.com>

--- weak-credentials/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = process.env.NJSPORT || 2222
-require('./configs/helmet')(app)
-app.use('/images', express.static('images'))
-app.use(morgan('combined'))
-require('./routes')(app)
-app.listen(port, () => { console.log(`Weak Credentials demo is listening on ${port}.`) })
