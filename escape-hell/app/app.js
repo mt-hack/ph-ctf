e1f068022f6656ccd2f177c353690050c884b193
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-12-24 23:38:10 +0800

    Minor changes to structure of JS Hell

--- escape-hell/app/app.js
+++ escape-hell/app/app.js
@@ -1,3 +1,4 @@
+// Inspired by AIS3 2019 Pre-exam
 const port = process.env.NJSPORT || 10000
 const flag = process.env.PHFLAG || 'flag is not set'
 const express = require('express')
@@ -7,19 +8,17 @@ const app = express()
 app.use(morgan(':remote-addr :method :url'))
 app.use('/', express.static('./'))
 
-class TokenError extends Error {}
+class FlagError extends Error {}
 
 app.get('/flag', (req, res) => {
   try {
     const token = req.query.token
 
-    if (!token) { throw new TokenError('You need to give us something to work with!') }
+    if (!token) { throw new FlagError('You need to give us something to work with!') }
 
-    if (token.length > 64) { throw new TokenError(`${token.length} is too long!`) }
-
-    if (0 < token.length < 32) { throw new TokenError('Nope, this ain\'t it.') }
+    if (0 < token.length < 32) { throw new FlagError('Nope, this ain\'t it.') }
   } catch (e) {
-    if (e instanceof TokenError) {
+    if (e instanceof FlagError) {
       res.send(e.message)
       return
     }
